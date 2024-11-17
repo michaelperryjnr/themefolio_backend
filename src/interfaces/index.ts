a5cb1adafa4cf9841c53fbb4e53ac853a57b4267
Nii Tettey <michaelperryt97@gmail.com>
2024-11-17 23:43:09 +0000

    Creaeted interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,39 @@
+interface DeviceInfo {
+  browser: string;
+  os: string;
+  device: string;
+}
+
+interface Session {
+  sessionId: string;
+  startTime: Date;
+  endTime?: Date;
+  pages: string[];
+  deviceInfo: DeviceInfo;
+}
+
+interface PageView {
+  path: string;
+  timestamp: Date;
+  userAgent: string;
+  ip: string;
+  referrer: string;
+  sessionId: string;
+}
+
+interface AnalyticsEvent {
+  sessionId: string;
+  eventType: string;
+  eventData: Record<string, unknown>;
+  timestamp: Date;
+}
+
+interface AnalyticsData {
+  totalPageViews: number;
+  totalSessions: number;
+  totalEvents: number;
+  popularPages: [string, number][];
+  averageSessionDuration: number;
+}
+
+export { DeviceInfo, Session, PageView, AnalyticsEvent, AnalyticsData };
