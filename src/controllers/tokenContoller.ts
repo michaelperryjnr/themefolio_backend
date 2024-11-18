ee2874077c58507bd1fe70de062bcb35dfaa72ef
Nii Tettey <michaelperryt97@gmail.com>
2024-11-18 12:06:32 +0000

    Created token controllers

--- src/controllers/tokenContoller.ts
+++ src/controllers/tokenContoller.ts
@@ -2,7 +2,9 @@ import jwt from "jsonwebtoken";
 import { Config } from "../core";
 
 async function generateAccessToken(username: string) {
-  return await jwt.sign({ username }, Config.JWT_SECRET);
+  return await jwt.sign({ username }, Config.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 }
 
 async function verifyAccessToken(token: string) {
