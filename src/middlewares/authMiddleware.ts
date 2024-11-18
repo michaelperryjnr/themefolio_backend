43fef57bcd816b61e0f2a8839f6802a2e6351031
Nii Tettey <michaelperryt97@gmail.com>
2024-11-18 12:07:19 +0000

    Created auth middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyAccessToken } from "../controllers/tokenContoller";
+
+interface JWTPayload {
+  username: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: JWTPayload;
+    }
+  }
+}
+
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).send("Access denied");
+  }
+
+  try {
+    const decoded = (await verifyAccessToken(token)) as JWTPayload;
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).send("Invalid token");
+  }
+};
+
+export default authMiddleware;
