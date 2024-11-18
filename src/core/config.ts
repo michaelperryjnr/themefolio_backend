7b432cd228ba51f9d131976dc1d5297203c2dd5b
Nii Tettey <michaelperryt97@gmail.com>
2024-11-18 12:06:47 +0000

    Created db cores

--- src/core/config.ts
+++ src/core/config.ts
@@ -3,16 +3,17 @@ config();
 import { CorsOptions } from "cors";
 
 const corsOptions: CorsOptions = {
-  origin: ["theniitettey.live"],
+  origin: ["theniitettey.live", "www.theniitettey.live", "localhost"],
   methods: ["GET", "POST"],
 };
 
 const Config = {
   JWT_SECRET: process.env.JWT_SECRET! || "shit_x_wavy",
   MONGO_URI: process.env.MONGO_URI!,
-  ADMIN_USERNAME: process.env.ADMIN_USERNAME! || "admin",
-  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || "admin@123",
+  ADMIN_USERNAME: "admin",
+  ADMIN_PASSWORD: "admin@123",
   PORT: process.env.PORT || 5000,
+  SALT_ROUNDS: process.env.SALT_ROUNDS! || 10,
   corsOptions: corsOptions,
 };
 
